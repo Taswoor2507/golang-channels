8ca54d9b2658ef7643d9568ac9f3c614bb7b04d2
Taswoor2507 <taswoorhossein72@gmail.com>
2025-02-12 16:57:45 +0500

    pratcice work

---
+++ main.go
@@ -0,0 +1,258 @@
+// // // package main
+
+// // // import (
+// // // 	"fmt"
+// // // 	"time"
+// // // )
+
+// // // func serveFood(table int) {
+// // // 	fmt.Println("Serving food to table", table)
+// // // }
+// // // func main() {
+// // // 	for i := 1; i < 1000; i++ {
+// // // 		go serveFood(i)
+// // // 	}
+// // // 	time.Sleep(1 * time.Second)
+// // // 	fmt.Println("Alltable served successfully")
+// // // }
+
+// // // package main
+
+// // // import (
+// // // 	"fmt"
+// // // 	"reflect"
+// // // 	"sync"
+// // // )
+
+// // // func serveFood(table int, wg *sync.WaitGroup) {
+// // // 	defer wg.Done()
+// // // 	fmt.Printf("%+v\n", wg)         // Print WaitGroup internal fields
+// // // 	fmt.Println(reflect.TypeOf(wg)) // Check the actual struct type
+// // // 	fmt.Println("Serving food to table----->", table, *wg)
+// // // }
+// // // func main() {
+// // // 	var wg sync.WaitGroup
+// // // 	for i := 1; i < 50000; i++ {
+// // // 		wg.Add(1)
+// // // 		go serveFood(i, &wg)
+
+// // // 	}
+// // // 	wg.Wait()
+// // // 	fmt.Println("All table served successfully")
+
+// // // }
+
+// // // defer
+// // // package main
+
+// // // import (
+// // // 	"fmt"
+// // // )
+
+// // // func main() {
+// // // 	fmt.Println("Hello")
+// // // 	defer fmt.Println("Bye")
+// // // 	defer fmt.Println("World")
+// // // }
+
+// // //
+
+// // // func serveDrinks(table int, wg *sync.WaitGroup) {
+// // // 	defer wg.Done() // Ensure Done is called even if the function panics
+// // // 	fmt.Println("Serving drinks to table", table)
+// // // }
+
+// // // func serveFood(table int, wg *sync.WaitGroup) {
+// // // 	defer wg.Done() // Ensure Done is called even if the function panics
+// // // 	fmt.Println("Serving food to table", table)
+// // // }
+
+// // // func main() {
+// // // 	var wg sync.WaitGroup
+// // // 	var wg2 sync.WaitGroup
+// // // 	// Launch 5 goroutines for serveDrinks
+// // // 	for i := 1; i <= 5; i++ {
+// // // 		wg.Add(1) // Add 1 for each goroutine
+// // // 		go serveDrinks(i, &wg)
+// // // 	}
+
+// // // 	// Launch 5 goroutines for serveFood
+// // // 	for i := 1; i <= 5; i++ {
+// // // 		wg2.Add(1) // Add 1 for each goroutine
+// // // 		go serveFood(i, &wg2)
+// // // 	}
+
+// // // 	// Wait for all goroutines to finish
+// // // 	wg.Wait()
+// // // 	fmt.Println("All tables served successfully")
+// // // }
+
+// // // https://go.dev/tour/concurrency/3
+
+// // package main
+
+// // import (
+// // 	"fmt"
+// // 	"time"
+// // )
+
+// // func reciever(ch chan int) {
+// // 	fmt.Println("Reciever: ", <-ch)
+// // 	// Blocks until a value is sent to ch
+
+// // }
+// // func main() {
+// // 	fmt.Println("Channels in golang ")
+// // 	time.Sleep(time.Second * 5)
+// // 	ch1 := make(chan int)
+// // 	go reciever(ch1) // Start a new goroutine to handle reciever function
+// // 	ch1 <- 1
+// // 	// fmt.Println(<-ch1) // Prints: 1
+// // 	fmt.Println("hello wrold ")
+// // }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"io"
+// 	"net/http"
+// )
+
+// func fetchURL(url string, ch chan<- string) {
+// 	resp, err := http.Get(url)
+// 	if err != nil {
+// 		ch <- fmt.Sprintf("Error fetching %s: %v", url, err)
+// 		return
+// 	}
+// 	defer resp.Body.Close() // Ensure response body is closed after reading
+
+// 	// Read the response body
+// 	body, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		ch <- fmt.Sprintf("Error reading body of %s: %v", url, err)
+// 		return
+// 	}
+
+// 	// Send response status and first 100 characters of the response body
+// 	ch <- fmt.Sprintf("Fetched %s with status: %s\nResponse Body (First 100 chars): %s\n",
+// 		url, resp.Status, string(body[:10000]))
+// }
+
+// func main() {
+// 	urls := []string{
+// 		"https://golang.org",
+// 		"https://google.com",
+// 		"https://github.com",
+// 	}
+
+// 	ch := make(chan string)
+// 	for _, url := range urls {
+// 		go fetchURL(url, ch)
+// 	}
+
+// 	// Receive and print responses
+// 	for range urls {
+// 		fmt.Println(<-ch)
+// 	}
+// }
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// sending
+func processNum(numchan chan int) {
+	for num := range numchan {
+
+		fmt.Println("processing number ..", num)
+		time.Sleep(time.Second)
+	}
+}
+
+// recieving
+//
+//	func sum(result chan int, num1 int, num2 int) {
+//		resultOf := num1 + num2
+//		result <- resultOf
+//	}
+//
+// goroutine synccronizer
+//
+//	func task(done chan bool) {
+//		defer func() { done <- true }()
+//		fmt.Println("processing task...")
+//	}
+
+// func emailSender(emailChan <-chan string, done chan<- bool) {
+// 	defer func() { done <- true }()
+// 	for email := range emailChan {
+// 		fmt.Println("Sending email to ", email)
+// 		time.Sleep(time.Second)
+// 	}
+// }
+
+func main() {
+	chan1 := make(chan int)
+	chan2 := make(chan string)
+
+	go func() {
+		chan1 <- 10
+	}()
+	go func() {
+		chan2 <- "pong"
+	}()
+
+	// combination
+	for i := 0; i < 2; i++ {
+		select {
+		case chan1Value := <-chan1:
+			fmt.Println("received data from channnel chan1", chan1Value)
+		case chan2Value := <-chan2:
+			fmt.Println("received data from channnel chan2", chan2Value)
+		}
+	}
+
+	// emailChan := make(chan string, 100) //buffered channel
+	// done := make(chan bool)
+	// go emailSender(emailChan, done)
+	// for i := 0; i < 5; i++ {
+	// 	emailChan <- fmt.Sprintf("%d@gmail.com", i)
+	// }
+	// close(emailChan)
+	// fmt.Println("done sending ")
+	// <-done
+
+	// emailChan <- "one@example.com"
+	// emailChan <- "two@example.com"
+	// emailChan <- "three@example.com"
+	// fmt.Println("chan data", <-emailChan)
+	// fmt.Println("chan data", <-emailChan)
+	// fmt.Println("chan data", <-emailChan)
+
+	// done := make(chan bool)
+	// go task(done)
+	// <-done //block jab tak data send nahi hota
+	// fmt.Println("end")
+	// result := make(chan int)
+	// go sum(result, 4, 5)
+	// res := <-result
+	// fmt.Println(res)
+
+	// numchan := make(chan int)
+	// go processNum(numchan)
+	// for {
+	// 	numchan <- rand.Intn(100)
+	// }
+	// numchan <- 10
+
+	// time.Sleep(time.Second)
+
+	// messageChan := make(chan string)
+	// messageChan <- "Hello word " //blocking
+	// msg := <-messageChan
+	// fmt.Println(msg)
+}
